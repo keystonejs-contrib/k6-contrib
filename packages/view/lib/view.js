d0107847670d873d61737be33234dc71c2cbb647
Gautam Singh <gautamsi@users.noreply.github.com>
2020-07-20 01:21:47 +0530

    upgrade keystone 11

--- packages/view/lib/view.js
+++ packages/view/lib/view.js
@@ -246,13 +246,8 @@ module.exports = class View {
 
     this.queryQueue.push(async () => {
       const callbacks = chain.callbacks;
-      const queryFn = this.keystone.executeQuery;
-      const context = this.keystone.getGraphQlContext({
-        schemaName: this.schemaName,
-        req: this.req,
-      });
       try {
-        const { data, errors } = await queryFn(query, { context, variables });
+        const { data, errors } = await this.keystone.executeGraphQL({ query, variables });
         locals[key] = data;
         const resultKeys = Object.keys(data || {});
         if (unwrap && resultKeys.length === 1) {
