8196c6a8d3a0bb156d0399bf38fff661c193924f
Gautam Singh <5769869+gautamsi@users.noreply.github.com>
2021-02-21 12:55:39 +0530

    fix editorjs building

--- packages/fields-editorjs/src/index.js
+++ packages/fields-editorjs/src/index.js
@@ -1,13 +1,15 @@
 import { Text } from '@keystonejs/fields';
-import { importView } from '@keystonejs/build-field-types';
 import { EditorJsImplementation } from './Implementation';
+
+const pkgDir = path.dirname(require.resolve('@keystonejs-contrib/fields-editorjs/package.json'));
+export const resolveView = pathname => path.join(pkgDir, pathname);
 
 export let EditorJs = {
   type: 'EditorJs',
   implementation: EditorJsImplementation,
   views: {
-    Controller: importView('./views/Controller'),
-    Field: importView('./views/Field'),
+    Controller: resolveView('views/Controller'),
+    Field: resolveView('views/Field'),
     Filter: Text.views.Filter,
   },
   adapters: Text.adapters,
