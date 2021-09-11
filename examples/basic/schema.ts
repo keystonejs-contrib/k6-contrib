14fef03b4207e7a3f2dd12dcb5ab3a430f3116ba
Gautam Singh <5769869+gautamsi@users.noreply.github.com>
2021-09-12 05:03:11 +0530

    add encrypted field

--- examples/basic/schema.ts
+++ examples/basic/schema.ts
@@ -11,6 +11,7 @@ import {
   file,
 } from '@keystone-next/keystone/fields';
 import { document } from '@keystone-next/fields-document';
+import { encrypted } from '@k6-contrib/fields-encrypted';
 import { configureTracking } from '@k6-contrib/list-plugins';
 
 // import { cloudinaryImage } from '@keystone-next/cloudinary';
@@ -39,157 +40,184 @@ const randomNumber = () => Math.round(Math.random() * 10);
 const withTracking = configureTracking({});
 
 export const lists = createSchema({
-  User: list(withTracking({
-    ui: {
-      listView: {
-        initialColumns: ['name', 'posts', 'avatar'],
+  User: list(
+    withTracking({
+      ui: {
+        listView: {
+          initialColumns: ['name', 'posts', 'avatar'],
+        },
       },
-    },
-    fields: {
-      /** The user's first and last name. */
-      name: text({ isRequired: true }),
-      /** Email is used to log into the system. */
-      email: text({ isRequired: true, isIndexed: 'unique' }),
-      /** Avatar upload for the users profile, stored locally */
-      avatar: image(),
-      attachment: file(),
-      /** Used to log in. */
-      password: password(),
-      /** Administrators have more access to various lists and fields. */
-      isAdmin: checkbox({
-        access: {
-          create: access.isAdmin,
-          read: access.isAdmin,
-          update: access.isAdmin,
-        },
-        ui: {
-          createView: {
-            fieldMode: args => (access.isAdmin(args) ? 'edit' : 'hidden'),
+      fields: {
+        /** The user's first and last name. */
+        name: text({ isRequired: true }),
+        /** Email is used to log into the system. */
+        email: text({
+          isRequired: true,
+          isIndexed: 'unique',
+          isFilterable: true,
+          isOrderable: true,
+        }),
+        secret: encrypted({
+          reverse: true,
+          secret: process.env.ENCRYPTION_KEYS || 'Super secret encryption keys for testing',
+        }),
+        superSecret: encrypted({
+          secret: process.env.ENCRYPTION_KEYS || 'Super secret encryption keys for testing',
+        }),
+        secret2: encrypted({
+          reverse: true,
+          ui: {displayMode: 'textarea'},
+          secret: process.env.ENCRYPTION_KEYS || 'Super secret encryption keys for testing',
+        }),
+        superSecret2: encrypted({
+          ui: {displayMode: 'textarea'},
+          secret: process.env.ENCRYPTION_KEYS || 'Super secret encryption keys for testing',
+        }),
+        /** Avatar upload for the users profile, stored locally */
+        avatar: image(),
+        attachment: file(),
+        /** Used to log in. */
+        password: password(),
+        /** Administrators have more access to various lists and fields. */
+        isAdmin: checkbox({
+          access: {
+            create: access.isAdmin,
+            read: access.isAdmin,
+            update: access.isAdmin,
           },
-          itemView: {
-            fieldMode: args => (access.isAdmin(args) ? 'edit' : 'read'),
-          },
-        },
-      }),
-      roles: text({}),
-      phoneNumbers: relationship({
-        ref: 'PhoneNumber.user',
-        many: true,
-        ui: {
-          // TODO: Work out how to use custom views to customise the card + edit / create forms
-          // views: './admin/fieldViews/user/phoneNumber',
-          displayMode: 'cards',
-          cardFields: ['type', 'value'],
-          inlineEdit: { fields: ['type', 'value'] },
-          inlineCreate: { fields: ['type', 'value'] },
-          linkToItem: true,
-          // removeMode: 'delete',
-        },
-      }),
-      posts: relationship({ ref: 'Post.author', many: true }),
-      randomNumber: virtual({
-        field: graphql.field({
-          type: graphql.Float,
-          resolve() {
-            return randomNumber();
+          ui: {
+            createView: {
+              fieldMode: args => (access.isAdmin(args) ? 'edit' : 'hidden'),
+            },
+            itemView: {
+              fieldMode: args => (access.isAdmin(args) ? 'edit' : 'read'),
+            },
           },
         }),
-      }),
-    },
-  })),
-  PhoneNumber: list(withTracking({
-    ui: {
-      isHidden: true,
-      // parentRelationship: 'user',
-    },
-    fields: {
-      label: virtual({
-        field: graphql.field({
-          type: graphql.String,
-          resolve(item) {
-            return `${item.type} - ${item.value}`;
+        roles: text({}),
+        phoneNumbers: relationship({
+          ref: 'PhoneNumber.user',
+          many: true,
+          ui: {
+            // TODO: Work out how to use custom views to customise the card + edit / create forms
+            // views: './admin/fieldViews/user/phoneNumber',
+            displayMode: 'cards',
+            cardFields: ['type', 'value'],
+            inlineEdit: { fields: ['type', 'value'] },
+            inlineCreate: { fields: ['type', 'value'] },
+            linkToItem: true,
+            // removeMode: 'delete',
           },
         }),
-        ui: {
-          listView: {
-            fieldMode: 'hidden',
+        posts: relationship({ ref: 'Post.author', many: true }),
+        randomNumber: virtual({
+          field: graphql.field({
+            type: graphql.Float,
+            resolve() {
+              return randomNumber();
+            },
+          }),
+        }),
+      },
+    })
+  ),
+  PhoneNumber: list(
+    withTracking({
+      ui: {
+        isHidden: true,
+        // parentRelationship: 'user',
+      },
+      fields: {
+        label: virtual({
+          field: graphql.field({
+            type: graphql.String,
+            resolve(item) {
+              return `${item.type} - ${item.value}`;
+            },
+          }),
+          ui: {
+            listView: {
+              fieldMode: 'hidden',
+            },
+            itemView: {
+              fieldMode: 'hidden',
+            },
           },
-          itemView: {
-            fieldMode: 'hidden',
+        }),
+        user: relationship({ ref: 'User.phoneNumbers' }),
+        type: select({
+          options: [
+            { label: 'Home', value: 'home' },
+            { label: 'Work', value: 'work' },
+            { label: 'Mobile', value: 'mobile' },
+          ],
+          ui: {
+            displayMode: 'segmented-control',
           },
-        },
-      }),
-      user: relationship({ ref: 'User.phoneNumbers' }),
-      type: select({
-        options: [
-          { label: 'Home', value: 'home' },
-          { label: 'Work', value: 'work' },
-          { label: 'Mobile', value: 'mobile' },
-        ],
-        ui: {
-          displayMode: 'segmented-control',
-        },
-      }),
-      value: text({}),
-    },
-  })),
-  Post: list(withTracking({
-    fields: {
-      title: text(),
-      // TODO: expand this out into a proper example project
-      // Enable this line to test custom field views
-      // test: text({ ui: { views: require.resolve('./admin/fieldViews/Test.tsx') } }),
-      status: select({
-        options: [
-          { label: 'Published', value: 'published' },
-          { label: 'Draft', value: 'draft' },
-        ],
-        ui: {
-          displayMode: 'segmented-control',
-        },
-      }),
-      content: document({
-        ui: { views: require.resolve('./admin/fieldViews/Content.tsx') },
-        relationships: {
-          mention: {
-            kind: 'inline',
-            label: 'Mention',
-            listKey: 'User',
+        }),
+        value: text({}),
+      },
+    })
+  ),
+  Post: list(
+    withTracking({
+      fields: {
+        title: text(),
+        // TODO: expand this out into a proper example project
+        // Enable this line to test custom field views
+        // test: text({ ui: { views: require.resolve('./admin/fieldViews/Test.tsx') } }),
+        status: select({
+          options: [
+            { label: 'Published', value: 'published' },
+            { label: 'Draft', value: 'draft' },
+          ],
+          ui: {
+            displayMode: 'segmented-control',
           },
-          featuredAuthors: {
-            kind: 'prop',
-            listKey: 'User',
-            many: true,
-            selection: `posts(first: 10) {
+        }),
+        content: document({
+          ui: { views: require.resolve('./admin/fieldViews/Content.tsx') },
+          relationships: {
+            mention: {
+              kind: 'inline',
+              label: 'Mention',
+              listKey: 'User',
+            },
+            featuredAuthors: {
+              kind: 'prop',
+              listKey: 'User',
+              many: true,
+              selection: `posts(first: 10) {
             title
           }`,
+            },
           },
-        },
-        formatting: true,
-        layouts: [
-          [1, 1],
-          [1, 1, 1],
-          [2, 1],
-          [1, 2],
-          [1, 2, 1],
-        ],
-        links: true,
-        dividers: true,
-        componentBlocks,
-      }),
-      publishDate: timestamp(),
-      author: relationship({
-        ref: 'User.posts',
-        ui: {
-          displayMode: 'cards',
-          cardFields: ['name', 'email'],
-          inlineEdit: { fields: ['name', 'email'] },
-          linkToItem: true,
-          inlineCreate: { fields: ['name', 'email'] },
-        },
-      }),
-    },
-  })),
+          formatting: true,
+          layouts: [
+            [1, 1],
+            [1, 1, 1],
+            [2, 1],
+            [1, 2],
+            [1, 2, 1],
+          ],
+          links: true,
+          dividers: true,
+          componentBlocks,
+        }),
+        publishDate: timestamp(),
+        author: relationship({
+          ref: 'User.posts',
+          ui: {
+            displayMode: 'cards',
+            cardFields: ['name', 'email'],
+            inlineEdit: { fields: ['name', 'email'] },
+            linkToItem: true,
+            inlineCreate: { fields: ['name', 'email'] },
+          },
+        }),
+      },
+    })
+  ),
 });
 
 export const extendGraphqlSchema = graphQLSchemaExtension({
